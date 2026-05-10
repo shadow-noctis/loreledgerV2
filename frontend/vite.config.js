b984943c626266608b7df7477ae49ab842ecd05e
cursed_noctis <noctisinmementos@gmail.com>
2026-05-10 13:29:47 +0300

    Docker setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // also needed so the port is exposed outside the container
+  }
 })
